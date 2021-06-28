4c31c5775fd69f24259c888d7b4a0589907bbe56
Ryan Yao <nienfeng@gmail.com>
2021-06-28 11:11:47 +0800

    add check_cuda.py to verify cv/torch use cuda or not.

---
+++ check_cuda.py
@@ -0,0 +1,27 @@
+import cv2, torch
+
+
+def check_cv2():
+	print("Check CV2:")
+	count = cv2.cuda.getCudaEnabledDeviceCount()
+	if count > 0:
+		print('\tusing cuda')
+	else:
+		print('\tnot using cuda')
+
+
+def check_torch():
+	print("Check Torch:")
+	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+	print('\tUsing device:', device)
+	#Additional Info when using cuda
+	if device.type == 'cuda':
+		print('\t%s' % torch.cuda.get_device_name(0))
+		print('\tMemory Usage:')
+		print('\tAllocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+		print('\tCached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
+
+
+if __name__ == '__main__':
+	check_cv2()
+	check_torch()
